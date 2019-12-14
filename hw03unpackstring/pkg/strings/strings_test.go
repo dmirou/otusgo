a6f57339bc896ecedf1dc072a480ac3c21bb0ca6
Dmitriy Neustroev <dneustroev@openprovider.ru>
2019-12-14 16:42:54 +0700

    hw3: add base test cases fro strings package

---
+++ hw03unpackstring/pkg/strings/strings_test.go
@@ -0,0 +1,45 @@
+package strings
+
+import (
+	"testing"
+
+	"github.com/dmirou/otusgo/hw03unpackstring/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+type Case struct {
+	In  string
+	Out CaseOut
+}
+
+type CaseOut struct {
+	Str string
+	Err error
+}
+
+func TestUnpack(t *testing.T) {
+	cases := []Case{
+		{
+			In:  "a4bc2d5e",
+			Out: CaseOut{Str: "aaaabccddddde", Err: nil},
+		},
+		{
+			In:  "abcd",
+			Out: CaseOut{Str: "abcd", Err: nil},
+		},
+		{
+			In:  "45",
+			Out: CaseOut{Str: "", Err: &errors.InvalidArgError{Value: "45"}},
+		},
+		{
+			In:  "",
+			Out: CaseOut{Str: "", Err: nil},
+		},
+	}
+
+	for _, testCase := range cases {
+		outStr, err := Unpack(testCase.In)
+		assert.EqualValues(t, testCase.Out.Str, outStr, "out strings are different")
+		assert.EqualValues(t, testCase.Out.Err, err, "out errors are different")
+	}
+}
