9560071d89e765dfe6ae87702845cd922e6547cf
Dmitriy Neustroev <dneustroev@openprovider.ru>
2020-02-03 09:52:10 +0700

    hw06channels: implement dispatcher.Run

--- hw06channels/dispatcher/dispatcher.go
+++ hw06channels/dispatcher/dispatcher.go
@@ -1,7 +1,55 @@
 package dispatcher
 
+import "fmt"
+
 type Task func() error
 
-func Run(task []Task, N int, M int) error {
+func Run(tasks []Task, N int, M int) error {
+
+	tasksCount := len(tasks)
+	tasksQueue := make(chan Task, N)
+	quitChan := make(chan bool)
+	resultsChan := make(chan error, N)
+
+	go tasksToQueue(quitChan, tasks, tasksQueue)
+	for i := 0; i < N; i++ {
+		go worker(quitChan, tasksQueue, resultsChan)
+	}
+
+	errorCount := 0
+	for i := 0; i < tasksCount; i++ {
+		if result := <-resultsChan; result == nil {
+			continue
+		}
+		errorCount++
+		if errorCount == M {
+			close(quitChan)
+			return fmt.Errorf("errors limit was reached (%d)", errorCount)
+		}
+	}
 	return nil
+}
+
+func tasksToQueue(quitChan <-chan bool, tasks []Task, tasksQueue chan<- Task) {
+	for _, task := range tasks {
+		select {
+		case <-quitChan:
+			close(tasksQueue)
+			return
+		default:
+			tasksQueue <- task
+		}
+	}
+	close(tasksQueue)
+}
+
+func worker(quitChan <-chan bool, tasksQueue <-chan Task, resultChan chan<- error) {
+	for task := range tasksQueue {
+		select {
+		case <-quitChan:
+			return
+		default:
+			resultChan <- task()
+		}
+	}
 }
--- hw06channels/dispatcher/dispatcher_test.go
+++ hw06channels/dispatcher/dispatcher_test.go
@@ -7,24 +7,52 @@ import (
 )
 
 type TestCase struct {
-	Tasks  []Task // tasks to execute
-	N      int    // count of workers
-	M      int    // max count of errors
-	Result error
+	Tasks       []Task // tasks to execute
+	N           int    // count of workers
+	M           int    // max count of errors
+	ReturnError bool
 }
 
 func TestRun(t *testing.T) {
 	testCases := []TestCase{
 		{
-			Tasks:  GenerateTasks(10, 0),
-			N:      2,
-			M:      0,
-			Result: nil,
+			Tasks:       GenerateTasks(1, 0),
+			N:           1,
+			M:           0,
+			ReturnError: false,
+		},
+		{
+			Tasks:       GenerateTasks(2, 0),
+			N:           1,
+			M:           1,
+			ReturnError: false,
+		},
+		{
+			Tasks:       GenerateTasks(2, 1),
+			N:           1,
+			M:           1,
+			ReturnError: true,
+		},
+		{
+			Tasks:       GenerateTasks(2, 2),
+			N:           1,
+			M:           1,
+			ReturnError: true,
+		},
+		{
+			Tasks:       GenerateTasks(10, 3),
+			N:           2,
+			M:           2,
+			ReturnError: true,
 		},
 	}
 
 	for _, testCase := range testCases {
 		err := Run(testCase.Tasks, testCase.N, testCase.M)
-		assert.Equalf(t, testCase.Result, err, "results not equal")
+		if testCase.ReturnError {
+			assert.Errorf(t, err, "result should be an error")
+		} else {
+			assert.Nilf(t, err, "result should be nil")
+		}
 	}
 }
