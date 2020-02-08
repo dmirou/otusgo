d37b7506d02064642caa34c7d88c679d64ec0378
Dmitriy Neustroev <dneustroev@openprovider.ru>
2020-02-08 12:38:12 +0700

    hw07copyfile: add ProgressCopier type

--- hw07copyfile/pkg/file/file.go
+++ hw07copyfile/pkg/file/file.go
@@ -1,5 +1,22 @@
 package file
 
+import (
+	"io"
+
+	"github.com/cheggaaa/pb"
+)
+
+type ProgressCopier struct {
+	progressWriter io.Writer
+	progressBar    *pb.ProgressBar
+}
+
+func newProgressCopier(progressWriter io.Writer) *ProgressCopier {
+	return &ProgressCopier{
+		progressWriter: progressWriter,
+	}
+}
+
 // Copy copies bytes from a specified path to a specified destination with a specified offset.
 // You can limit count of bytes to be copied with the limit param.
 // Limit equals zero means that we will skip it.
@@ -8,6 +25,6 @@ package file
 // Copy returns nil if bytes were copied successfully.
 // It returns error if It can't define a file size to copy from.
 // It returns error if offset greater than the source file size and there is nothing to copy.
-func Copy(from string, to string, limit int, offset int) error {
+func (pc ProgressCopier) Copy(from string, to string, limit int, offset int) error {
 	return nil
 }
